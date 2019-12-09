754ff6a0f1f3c5ead8005d9b5cbf0d814dc25669
Maria Le <hp.mars.le@gmail.com>
2019-12-09 15:00:44 -0600

    add form to signup view and signup routes to appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,15 @@ class ApplicationController < Sinatra::Base
     end
     get '/' do
         "<h1>Hello World</h2>"
+        '<p><a>Login</a> or <a>Signup</a></p>'
     end 
 
+    get '/signup' do 
+        erb :signup 
+    end
+    
+    post '/signup' do 
+        byebug
 
+    end
 end
