a8bad539dc9ef0327c6f4205c5f0b48b6c7994e0
Maria Le <hp.mars.le@gmail.com>
2019-12-14 14:57:02 -0600

    connect additional routes and views

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,8 +1,60 @@
 class ExercisesController < ApplicationController
     get '/exercises' do 
-        erb :'exercise/index'
+        if logged_in?
+            if current_user.exercises
+                @exercises = current_user.exercises
+                erb :'/exercise/index'
+            else 
+                redirect '/exercises/new'
+            end
+        else 
+            redirect '/login'
+        end
+        
     end
 
+    get '/exercises/new' do 
 
+        erb :'exercise/new'
+    end
 
+    post '/exercises' do 
+        exercise = Exercise.new(exercise_params)
+        if exercise.save #1:12:44 in authenticAtion yt video
+            current_user.exercises << exercise
+            redirect "/exercises/#{current_user.id}" #user.exercises.build()? save the user
+        else
+            puts "ooohhhh nooooooooooooooo"
+        end
+        # current_user.exercises << @exercise
+    end
+
+    get '/exercises/:id' do 
+        @exercise = Exercise.find_by_id(params[:id])
+        erb :'exercise/show'
+    end
+
+    get '/exercises/:id/edit' do 
+
+        erb :edit
+    end 
+
+    patch '/aricles/:id' do 
+
+    end
+
+    put '/articles/:id' do 
+
+    end
+
+    delete '/articles/:id' do 
+
+    end
+
+    private
+    
+    def exercise_params
+        {name: params[:name], muscle_group: params[:muscle_group], length_of_time: params[:length_of_time], weight: params[:weight], sets: params[:sets], reps: params[:reps], notes: params[:notes]}
+        # params.require()
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-    get '/testing' do
-        "<h1>Hello World Again</h1>"
-    end 
+
     get '/signup' do 
         erb :'user/signup'
     end
@@ -9,17 +7,19 @@ class UsersController < ApplicationController
     
     post '/signup' do 
         @user = User.new(params)
-        @user.save
-        session[:user_id] = @user.id
-        login(params[:username], params[:password])
-        redirect '/exercises'
+        if @user.save
+            login(params[:username], params[:password])
+            erb :'exercise/new'
+        else
+            redirect '/signup'
+        end
     end
        
 
     get '/login' do 
-
         erb :'user/login'
     end
+
     post '/login' do 
         # binding.pry
         login(params[:username],params[:password])
