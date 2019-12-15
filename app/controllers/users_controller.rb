a4d24cdf5c30a6bc2f287ada524b457980007808
Maria Le <hp.mars.le@gmail.com>
2019-12-14 21:54:33 -0600

    add logout feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
     get '/signup' do 
         erb :'user/signup'
     end
-
     
     post '/signup' do 
         @user = User.new(params)
@@ -13,8 +12,7 @@ class UsersController < ApplicationController
         else
             redirect '/signup'
         end
-    end
-       
+    end  
 
     get '/login' do 
         erb :'user/login'
@@ -25,4 +23,9 @@ class UsersController < ApplicationController
         login(params[:username],params[:password])
         redirect '/exercises'
     end 
+
+    get '/logout' do
+        session.clear
+        redirect to '/'
+    end
 end
