b43c22adf383b6e119429b717b0cddd226c29344
Maria Le <hp.mars.le@gmail.com>
2019-12-15 18:06:53 -0600

    edit views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base #[6.0] why does this not work with version?]
     has_many :exercises
     has_secure_password
+    validates_presence_of :name, :email, :password, :username
     validates_uniqueness_of :username
 end
+
+ 
