5f7cd82a786580570516233d290002a5466e6016
Maria Le <hp.mars.le@gmail.com>
2020-09-28 23:55:54 -0500

    delete comments and whitespace

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,3 @@
 require 'bundler/setup'
 Bundler.require
-
-# require_relative '../app/models/user.rb'
-# require_relative '../app/controllers/application_controller.rb'
-# require_relative '../app/controllers/users_controller.rb'
-
 require_all 'app'
